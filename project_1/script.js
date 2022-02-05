77024e9f5fccd27b8f1200fce51152b5cb3c7da5
EStrukov <Jhezer1991@gmail.com>
2022-02-05 17:51:07 +0300

    second homework

--- project_1/script.js
+++ project_1/script.js
@@ -1,21 +1,43 @@
 'use strict';
 
-const numderOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+if (numberOfFilms < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+  alert("Вы классический зритель");
+} else if (numberOfFilms >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
+
+/*
+  (numberOfFilms < 10) ? alert("Просмотрено довольно мало фильмов") :
+  (numberOfFilms < 30) ? alert("Вы классический зритель"):
+  (numberOfFilms > 30) ? alert("Вы киноман") : alert("Произошла ошибка");
+*/
+
+1
 const personalMovieDB = {
-  count: numderOfFilms,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done")
+  } else {
+    console.log("error")
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
